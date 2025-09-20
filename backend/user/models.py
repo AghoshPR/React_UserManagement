01b24b155ee365b4f7bd37414b373ddd44c50e26
Aghosh <aghosh@gmail.com>
2025-09-20 10:47:40 +0530

    reg_login_finished

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class CustomUser(AbstractUser):
+
+    phone = models.CharField(max_length=12,blank=True,null=True)
+    profile_image = models.ImageField(upload_to='profiles/',blank=True,null=True)
+
+    def __str__(self):
+        return self.username
+
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from .serializers import *
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+
+
+
+class RegisterView(APIView):
+
+    permission_classes = [AllowAny]
+
+    def post(self,request):
+
+
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+
+            user=serializer.save()
+            return Response({"message":"User registered successfully"},status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+
+class LoginView(APIView):
+
+    permission_classes=[AllowAny]
+
+    def post(self,request):
+
+        username=request.data.get("uname")
+        password = request.data.get("pass")
+
+        user = authenticate(username=username,password=password)
+
+        if user is not None:
+
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                "refresh":str(refresh),
+                "access":str(refresh.access_token),
+                "username":user.username,
+                "email":user.email
+            })
+       
+        return Response({"error":"invalid Credentials"},status=status.HTTP_401_UNAUTHORIZED)
+
+
